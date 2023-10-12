dfb414e63513ddb41182f9f5204a9aaab1e5f7fe
Slim00shh <msilimmer@proton.me>
2023-10-12 09:09:05 -0600

    Updated env variable (notfixed yet)
    i tried to update and secure the api keys used

--- orgfunc.js
+++ orgfunc.js
@@ -17,19 +17,20 @@ function menudiv(eleMent){
 }
 
 function fetchstring(){
-  $("#")
+  $("#type-Selector").val();
+  $("#state-Selector").val();
+  $("#county-Selector").val();
+  $("#county-Selector").val();
   return ;
 }
 
-  /*Secret API */
-  /* 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 */
 
 function OpenAifetchApi(){
 
   console.log("Contancting openai...");
   var querie= fetchstring();
   var url="https://api.openai.com/v1/chat/completions";
-  var bearer = 'Bearer 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' ;
+  var bearer = 'Bearer ${API KEY}';
     fetch(url, {
         method: 'POST',
         headers: {
@@ -38,7 +39,8 @@ function OpenAifetchApi(){
         },
         body: JSON.stringify({
             "model":"gpt-3.5-turbo",
-            "messages": [{"role": "system", "content": ""},
+            "messages": [{"role": "system", 
+              "content": "Given the following SQL tables, your job is to write queries given a user’s request in spanish.\n\nCREATE TABLE estados (\n  IDestados int NOT NULL,  Nombre varchar NOT NULL,\n  Descripción varchar(30) NOT NULL,\nPRIMARY KEY (IDestados)\n);\n\nCREATE TABLE lugares turisticos (\n  IDlugares_turisticos int NOT NULL,\n  Horario_cierre varchar NOT NULL,\n  Clima varchar(20) NOT NULL,\n  Precios varchar(20) NOT NULL,\n  Tipo_ambiente varchar(30) NOT NULL,\n  Valoracion_lugar varchar(30) NOT NULL,\n  Lugares_populares varchar(30) NOT NULL,\nPRIMARY KEY(IDlugares_turisticos)\n);\n\nCREATE TABLE `municipios` (\n  `IDmunicipios` int(11) NOT NULL,\n  `Nombre` varchar(30) NOT NULL,\n  `Clima` varchar(30) NOT NULL\n);"},
               {"role": "user", "content": querie}],
             "temperature": 0,
             "max_tokens": 1024
